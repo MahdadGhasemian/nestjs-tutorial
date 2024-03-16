e070ec74e844f6d666483a2bb137db6e3a61f8ee
Mahdad Ghasemian <mahdad.ghasemian@gmail.com>
2024-03-16 14:24:09 +0330

    e2e testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
+import { APP_PIPE } from '@nestjs/core';
 import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,16 +8,24 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
-import { APP_PIPE } from '@nestjs/core';
 import * as session from 'express-session';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Report],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          entities: [User, Report],
+          synchronize: true,
+        };
+      },
     }),
     UsersModule,
     ReportsModule,
--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -5,6 +5,7 @@ import { AppModule } from './../src/app.module';
 
 describe('Authentication System (e2e)', () => {
   let app: INestApplication;
+  const random = Math.floor(Math.random() * 99999);
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -16,9 +17,9 @@ describe('Authentication System (e2e)', () => {
   });
 
   it('handles a signup request', () => {
-    const random = Math.floor(Math.random() * 99999);
     const userEmail = `test-${random}@test.com`;
-    const userPassword = `$random`;
+    const userPassword = `${random}`;
+
     return request(app.getHttpServer())
       .post('/auth/signup')
       .send({
@@ -31,5 +32,27 @@ describe('Authentication System (e2e)', () => {
         expect(id).toBeDefined();
         expect(email).toEqual(userEmail);
       });
+  });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const userEmail = `test-${random}@test.com`;
+    const userPassword = `${random}`;
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({
+        email: userEmail,
+        password: userPassword,
+      })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(userEmail);
   });
 });
